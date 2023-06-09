e239b9fd2725d6067ad4dc534ca2c8ada5fb3c55
AlienTuna <101868546+AlienTuna@users.noreply.github.com>
2023-06-09 22:44:39 +0300

    added axios lib

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import Notiflix from "notiflix";
-// import Axios from "axios";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.css";
 import ImagesApi from "./js/images-api";
 
-// const axios = require('axios').default;
 
 const refs = {
     searchForm: document.querySelector('.js-search-form'),
@@ -15,11 +13,12 @@ const refs = {
 const imgApi = new ImagesApi();
 const lightbox = new SimpleLightbox('.photo-card');
 Notiflix.Notify.init({
-    width: '380px',
+    width: '100%',
+    position: 'center-center',
     borderRadius: '2px',
     clickToClose: true,
-    opacity: 0.8,
-    fontSize: '18px',
+    opacity: 0.95,
+    fontSize: '20px',
     warning: {
         background: '#cf9f02',
         textColor: '#000',
@@ -27,6 +26,10 @@ Notiflix.Notify.init({
     failure: {
         background: '#da5f52',
         textColor: '#000',
+      },
+    success: {
+        background: '#225469',
+        textColor: '#c7b75b',
       },
 })
 
@@ -38,7 +41,7 @@ refs.moreButton.classList.add('hidden');
 async function onSearchSubmit(e) {
     e.preventDefault();
     
-    refs.moreButton.classList.add('hidden');
+    disableMoreBtn();
 
     clearGallery();
     imgApi.resetPage();
@@ -46,44 +49,63 @@ async function onSearchSubmit(e) {
     try {
         const resultJson = await imgApi.fetchImages()
         if (resultJson.length < 1) {
-            Notiflix.Notify.warning("Oops! There are no images matching your search query.")
-            
+            Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.")
             return
         }
-        renderGallery(resultJson)
-        refs.moreButton.classList.remove('hidden');
+        renderGallery(resultJson);
+        enableMoreBtn();
+        Notiflix.Notify.success(`Hooray! We found ${imgApi.hits} images.`);
     } catch {
         Notiflix.Notify.failure("Something went wrong. Try to reload the page.")
-        
         return
     }
 
 }
 
 async function onLoadMore() {
-    const resultJson = await imgApi.fetchImages()
-    renderGallery(resultJson);
+    try {
+        const resultJson = await imgApi.fetchImages()
+        if (resultJson.length < 1) {
+            Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
+            disableMoreBtn();
+            return;
+        }
+        renderGallery(resultJson);
+        enableMoreBtn();
+
+        const { height: cardHeight } = refs.resultContainer
+        .firstElementChild.getBoundingClientRect();
+        
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: "smooth",
+      });
+
+    } catch {
+        Notiflix.Notify.failure("Something went wrong. Try to reload the page.")
+        return
+    }
 
 }
 
 function renderGallery(imagesData) {
     const markup = imagesData.map(img => `
     <div href="${img.largeImageURL}" class="photo-card">
-    <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
-    <div class="info">
-        <p class="info-item">
-        <b>Likes: </b></br>${img.likes}
-        </p>
-        <p class="info-item">
-        <b>Views: </b></br>${img.views}
-        </p>
-        <p class="info-item">
-        <b>Comments: </b></br>${img.comments}
-        </p>
-        <p class="info-item">
-        <b>Downloads: </b></br>${img.downloads}
-        </p>
-    </div>
+        <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
+        <div class="info">
+            <p class="info-item">
+            <b>Likes: </b></br>${img.likes}
+            </p>
+            <p class="info-item">
+            <b>Views: </b></br>${img.views}
+            </p>
+            <p class="info-item">
+            <b>Comments: </b></br>${img.comments}
+            </p>
+            <p class="info-item">
+            <b>Downloads: </b></br>${img.downloads}
+            </p>
+        </div>
 </div>
     `).join('')
     refs.resultContainer.insertAdjacentHTML('beforeend', markup)
@@ -92,4 +114,10 @@ function renderGallery(imagesData) {
 
 function clearGallery() {
     refs.resultContainer.innerHTML = '';
+}
+function disableMoreBtn() {
+    refs.moreButton.classList.add('hidden');
+}
+function enableMoreBtn() {
+    refs.moreButton.classList.remove('hidden');
 }
--- src/js/images-api.js
+++ src/js/images-api.js
@@ -1,31 +1,58 @@
-// import axios from "axios";
-
 const axios = require('axios').default;
 const API_KEY = '29596647-f7db787be5835d1b0c2ce2eda'
 const API_URL = 'https://pixabay.com/api/'
 const PER_PAGE = 40
-const SET = '&image_type=photo&orientation=horizontal&safesearch=true&category=backgrounds'
+// const SET = '&image_type=photo&orientation=horizontal&safesearch=true&category=backgrounds'
 
 export default class ImagesApi {
-constructor() {
-    this.searchQuery = '';
-    this.page = 1;
-}
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+        this.totalHits = '';
+    }
 
- async fetchImages() {
-    const url = `${API_URL}?key=${API_KEY}&q=${this.searchQuery}&page=${this.page}&per_page=${PER_PAGE}${SET}`;
-    const result = await fetch(url);
-     const data = await result.json();
-     this.page += 1;
-     return data.hits;
-}
-resetPage() {
-    this.page = 1;
-}
-get query() {
-    return this.searchQuery;
-}
-set query(newQuery) {
-    this.searchQuery = newQuery;
-}
+    async fetchImages() {
+        try {
+            const response = await axios.get(API_URL, {
+                params: {
+                    key: API_KEY,
+                    q: this.searchQuery,
+                    page: this.page,
+                    per_page: PER_PAGE,
+                    image_type: 'photo',
+                    orientation: 'horizontal',
+                    safesearch: 'true',
+                }
+            }
+            )
+    
+            this.page += 1;
+            this.totalHits = response.data.totalHits;
+            return response.data.hits;
+            
+        } catch (error) {
+            console.log(error);         
+        }
+
+    }
+    //  async fetchImages() {
+    //     const url = `${API_URL}?key=${API_KEY}&q=${this.searchQuery}&page=${this.page}&per_page=${PER_PAGE}${SET}`;
+    //     const result = await fetch(url);
+    //     const data = await result.json();
+    //     this.page += 1;
+    //     this.totalHits = data.totalHits;
+    //     return data.hits;
+    // }
+    resetPage() {
+        this.page = 1;
+    }
+    get query() {
+        return this.searchQuery;
+    }
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+    get hits() {
+        return this.totalHits;
+    }
 }
