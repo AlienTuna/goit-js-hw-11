add64df02fa445ba559dca6d9e3573e3c0f8cf94
AlienTuna <101868546+AlienTuna@users.noreply.github.com>
2023-06-13 17:29:00 +0300

    Update refs.js

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,5 +2,5 @@ export default refs = {
     searchForm: document.querySelector('.js-search-form'),
     moreButton: document.querySelector('.more-button'),
     resultContainer: document.querySelector('div.js-result-container'),
-    resultFooter: document.querySelector('.result-footer'),
+    resultFooter: document.querySelector('.result-footer')
 }
